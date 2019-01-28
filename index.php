9f63c2feefe39c5621393d160bd6351022c36149
Unknown <jordan.merrien06@gmail.com>
2019-01-28 16:56:12 +0100

    Nouveau Header :3

--- index.php
+++ index.php
@@ -50,31 +50,15 @@
             </div> <!-- /#wrapper -->
 
 
-
-
-
-
-            <!-- Bootstrap core JavaScript -->
-            <script src="js/jquery.min.js"></script>
-            <script src="js/bootstrap.bundle.min.js"></script> <!-- Menu Toggle Script -->
             <script>
-              $(function(){
-                $("#menu-toggle").click(function(e) {
-                    e.preventDefault();
-                    $("#wrapper").toggleClass("toggled");
-                });
-    
-                $(window).resize(function(e) {
-                  if($(window).width()<=768){
-                    $("#wrapper").removeClass("toggled");
-                  }else{
-                    $("#wrapper").addClass("toggled");
-                  }
-                });
-              });
-               
-            </script>
+            function openNav() {
+            document.getElementById("mySidenav").style.width = "250px";
+            }
 
+            function closeNav() {
+            document.getElementById("mySidenav").style.width = "0";
+            }
+            </script>
     </body>
 
 
