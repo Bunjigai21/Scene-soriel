2975104564285c94b0fc36853164948ce2b32311
Unknown <jordan.merrien06@gmail.com>
2019-02-25 10:24:16 +0100

    Page Merci

---
+++ merci.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Histoire</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        
+                
+    </head>
+
+    <body id="merci">
+
+    
+        <div id="page-content-wrapper">
+            <div class="container-fluid">
+                <h1>MERCI POUR VOTRE PARTICIPATION !</h1>
+                <p class="p-merci">N'hésitez pas à revenir pour voir l'évolution de l'installation !</p>
+                
+            </div>
+        </div>
+
+
+
+            <?php
+            include_once('footer.html');
+            ?>
+</body>
+
+
+<footer>
+
+
+</footer>
+
+
+</html>
